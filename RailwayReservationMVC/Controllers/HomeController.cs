82aeefe038225636f4eaa234136eeac225f0b5b7
akshbedarkar <akshata26bedarkar@gmail.com>
2022-12-16 07:49:03 +0000

    added signup and homepage functionality

--- RailwayReservationMVC/Controllers/HomeController.cs
+++ RailwayReservationMVC/Controllers/HomeController.cs
@@ -1,26 +1,56 @@
 ﻿using RailwayReservationMVC.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using System.Text;
+using RailwayReservationMVC.Models.DAL;
 
 namespace RailwayReservationMVC.Controllers
 {
     public class HomeController : Controller
     {
-        DataContext obj = new DataContext();
-        //homepage
-        public ActionResult Index()
+        #region Repository Object
+        private IRailwayRepository<Reservation> ReservationObject;
+
+        private IRailwayRepository<TrainDetails> TrainDetailsObject;
+        private IRailwayRepository<User> UserObject;
+        #endregion
+
+
+        #region Repository Constructor
+        public HomeController()
         {
-            var data = obj.TrainDetails.ToList();
+            this.ReservationObject = new RailwayRepository<Reservation>();
+            this.TrainDetailsObject = new RailwayRepository<TrainDetails>();
+            this.UserObject = new RailwayRepository<User>();
+
+        }
+        #endregion
+
+
+        #region Homepage
+        public ActionResult Homepage()
+        {
+            var data = TrainDetailsObject.GetModel().ToList();
             return View(data);
         }
 
-        //signup page
+        #endregion
+
+
+
+        #region Search Train
+
+      
+       
+
+        #endregion
+
+
+
+        #region SignUp and Email Notification
         public ActionResult SignUp()
         {
             return View();
@@ -29,18 +59,11 @@ namespace RailwayReservationMVC.Controllers
         [HttpPost]
         public ActionResult SignUp(User u)
         {
-            // database updation
-            var data = obj.Users.Add(u);
-            u.Password= Convert.ToBase64String(
-            System.Security.Cryptography.SHA256.Create()
-            .ComputeHash(Encoding.UTF8.GetBytes(u.Password)));
-            obj.SaveChanges();
-
             //email notification
             MailMessage mm = new MailMessage("railwayreservationsystemmail@gmail.com", u.Email);
 
             mm.Subject = "Welcome to Railway Reservation System";
-            mm.Body = "This is your password :" + u.Password.ToString();
+            mm.Body = "This is your password :" + u.Password.ToString() + "\n" + "Have a safe journey with us !";
             mm.IsBodyHtml = false;
             SmtpClient smtp = new SmtpClient();
             smtp.Host = "smtp.gmail.com";
@@ -50,26 +73,148 @@ namespace RailwayReservationMVC.Controllers
             NetworkCredential nc = new NetworkCredential("railwayreservationsystemmail@gmail.com", "chfxpbtcfjfobhlv");
             smtp.UseDefaultCredentials = true;
             smtp.Credentials = nc;
-
             smtp.Send(mm);
+            TempData["Message"] ="Thank you for Connecting with us!Your password has been sent to your regsitered mail id  ";
 
-            ViewBag.message = "Thank you for Connecting with us!Your password has been sent to your regsitered mail id  ";
+            //password encryption 
+            u.Password = Convert.ToBase64String(
+            System.Security.Cryptography.SHA256.Create()
+            .ComputeHash(Encoding.UTF8.GetBytes(u.Password)));
 
-            return RedirectToAction("Index");
+            //update database with new user
+            UserObject.InsertModel(u);
+            UserObject.Save();
+            return RedirectToAction("afterlogin");
+        }
+
+        #endregion
+
+
+
+        #region Login
+        public ActionResult Login()
+        {
+            return View();
         }
 
         
-        public ActionResult LogIn()
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                var data = UserObject.GetModel().Where(model => model.Email.Equals(email) && model.Password.Equals(password)).ToList();
+                if (data.Count() > 0)
+                { 
+                    return RedirectToAction("afterlogin");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("Login");
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult AdminLogin()
+        {
+            return View("");
+        }
+        [HttpPost]
+        public ActionResult AdminLogin(string email, string password, string role)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var data = UserObject.GetModel().Where(model => model.Email.Equals(email) && model.Password.Equals(password) && model.Role.Equals(role)).ToList();
+                if (data.Count() > 0)
+                {
+                    
+                    return RedirectToAction("Updatetrain");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("AdminLogin");
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult afterlogin()
+        {
+            return View();
+        }
+
+        #endregion
+
+
+
+        #region logout
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+        #endregion
+
+
+        #region Reservation
+
+        public ActionResult ReservationPage()
+        {
+            var data = ReservationObject.GetModel();
+            return View(data);
+        }
+        public ActionResult CreateReservation()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult LogIn(User u)
+        public ActionResult CreateReservation(Reservation res)
+
         {
-            return View();
+
+            var data = ReservationObject.GetModel();
+            if (data.Count() >= 7)
+            {
+                TempData["msg"] = "<script>alert('YOU CANNOT ADD MORE THAN 6 PASSENGER');</script>";
+            }
+            else
+            {
+
+                ReservationObject.InsertModel(res);
+                ReservationObject.Save();
+
+                return RedirectToAction("ReservationPage");
+            }
+            return View(res);
+
         }
+
+        public ActionResult DeleteReservation(int id)
+        {
+            Reservation rt = ReservationObject.GetModelByID(id);
+            return View(rt);
+        }
+        [HttpPost]
+        public ActionResult DeleteReservation(int id, Reservation reservation)
+        {
+
+            ReservationObject.DeleteModel(id);
+            ReservationObject.Save();
+            return RedirectToAction("ReservationPage");
+        }
+
+        #endregion
+
 
 
     }
 }
+  
